1363bccf3b9b525ef86ae76a4fdf1b86e066ab05
Tiago Almeida <almeidatiago56@gmail.com>
2016-11-17 22:13:31 +0000

    clean up code

--- SceneManagement/lab1/src/main.cpp
+++ SceneManagement/lab1/src/main.cpp
@@ -46,16 +46,25 @@ int WinX = 640, WinY = 480;
 int WindowHandle = 0;
 unsigned int FrameCount = 0;
 
-Mesh meshTriangle;
-Mesh meshCube;
-Mesh meshParallelogram;
+Mesh meshTriangle, meshCube, meshParallelogram;
+
+Shader shader;
 
 SceneGraph scenegraph;
 
 GLuint ProgramId;
 GLint Camera_UId;
 
-Shader shader;
+Vec3 eye = { 0, 0, 8 };
+Vec3 center = { 0, 0, 0 };
+Vec3 up = { 0, 1, 0 };
+
+Mat4 viewMatrix = lookAt(eye, center, up);
+Mat4 perspectiveMatrix = perspective(30, (float)640 / 480, 1, 10);
+
+qtrn q;
+int frameRotationX, frameRotationY;
+int last_time;
 
 SceneNode* figure;
 SceneNode * cubeT;
@@ -91,7 +100,7 @@ Vec3 groundTranslation;
 
 
 
-/////////////////////////////////////////////////////////////////////// ERRORS
+////////////////////////////////////////// ERRORS
 
 bool isOpenGLError() {
 	bool isError = false;
@@ -221,8 +230,6 @@ void createShaderProgram()
 		//TODO
 		Camera_UId = GetUniformLocation(ProgramId, "Camera");
 	}
-
-	checkOpenGLError("ERROR: Could not create shaders.");
 }
 
 void destroyShaderProgram()
@@ -243,75 +250,6 @@ void destroyMeshes()
 	checkOpenGLError("ERROR: Could not destroy VAOs and VBOs.");
 }
 
-/////////////////////////////////////////////////////////////////////// SCENE
-
-Vec3 eye = { 0, 0, 8 };
-Vec3 center = { 0, 0, 0 };
-Vec3 up = { 0, 1, 0 };
-
-Mat4 viewMatrix = lookAt(eye, center, up);
-
-Mat4 orthoMatrix = ortho(-2, 2, -2, 2, 1, 10);
-Mat4 perspectiveMatrix = perspective(30, (float)640 / 480, 1, 10);
-Mat4* currentProjection = &perspectiveMatrix;
-
-//
-//void drawTriangles() {
-//	vector <int> faceElements = { 3,3,6,6,6 };
-//
-//	vector<int> colors = { 12, 13, 14, 15, 16 };
-//	GLGameObject triangle3d = GLGameObject(colors, 0, faceElements, UniformId, UniformColorId, (big_triangle_1_translate * rotateRight * scaleBig));
-//	triangle3d.draw();
-//	
-//	vector<int> colors2 = { 17, 18, 19, 20, 21 };
-//	GLGameObject triangle2 = GLGameObject(colors2, 0, faceElements, UniformId, UniformColorId, (big_triangle_2_translate * rotate90Left * scaleBig));
-//	triangle2.draw();
-//
-//	vector<int> colors3 = { 22, 23, 24, 25, 26 };
-//	GLGameObject triangle3 = GLGameObject(colors3, 0, faceElements, UniformId, UniformColorId, (translateMediumTriangle * rotateRight * scaleMedium));
-//	triangle3.draw();
-//
-//	vector<int> colors4 = { 27, 28, 29, 30, 31 };
-//	GLGameObject triangle4 = GLGameObject(colors4, 0, faceElements, UniformId, UniformColorId, (smallTriangle1Translate * scaleSmall));
-//	triangle4.draw();
-//
-//	vector<int> colors5 = { 32, 33, 34, 35, 36 };
-//	GLGameObject triangle5 = GLGameObject(colors5, 0, faceElements, UniformId, UniformColorId, (smallTriangle2Translate * scaleSmall));
-//	triangle5.draw();
-//
-//}
-//
-//
-//void drawSquares() {
-//	vector<int> vec = { 0,1,2,3,4,5 };
-//	vector <int> faceElements = { 6,6,6,6,6,6 };
-//
-//	GLGameObject cube = GLGameObject(vec, SQUARE_INDEX, faceElements, UniformId, UniformColorId, translate25Bottom);
-//	cube.draw();
-//}
-//
-//void drawParalelogram() {
-//	vector<int> colors = { 6,7,8,9,10,11 };
-//	vector <int> faceElements = { 6,6,6,6,6,6 };
-//
-//	GLGameObject paralellogram = GLGameObject(colors, PARALLELOGRAM_INDEX, faceElements, UniformId, UniformColorId, parallelogramTranslate * scaleBig);
-//	paralellogram.draw();
-//}
-
-bool gimbalLock = false;
-float xDegrees = 0;
-float yDegrees = 0;
-float zDegrees = 0;
-
-qtrn q;
-
-#define OFFSET_CAMERA 4.5f
-#define TO_RAD(f) (f * (PI / 180))
-
-int frameRotationX;
-int frameRotationY;
-int last_time;
-
 void updateAnimatedValues(int delta) {
 	animation_ms += delta;
 	for (size_t i = 0; i < animations.size(); i++)
@@ -329,7 +267,6 @@ void updateAnimatedValues(int delta) {
 void setViewProjectionMatrix()
 {
 	scenegraph.getCamera()->setViewMatrix(viewMatrix * q.toMatrix());
-
 }
 
 
@@ -413,24 +350,15 @@ void timer(int value)
 	glutTimerFunc(1000, timer, 0);
 }
 
-/////////////////////////////////////////////////////////////////////// SETUP
+int last_mouse_x, last_mouse_y;
 
-int clicked = -1;
-int mouseX;
-int mouseY;
 
+#define MOVE_OFFSET 0.1f
+#define OFFSET_CAMERA 4.5f
 #define EYE_OFFSET 0.3f
 
-//button 0 LEFT
-//button 1 MIDDLE
-//button 2 RIGHT
-//button 3 mwhell up
-//button 4 mwhell down
 void onMouse(int button, int state, int x, int y)
 {
-	if (state == 0) {
-		return;
-	}
 	if (button == 3) {
 		eye.z -= EYE_OFFSET;
 	}
@@ -441,24 +369,23 @@ void onMouse(int button, int state, int x, int y)
 }
 
 void onMotion(int x, int y) {
-	if (x > mouseX) {
+	if (x > last_mouse_x) {
 		frameRotationY += OFFSET_CAMERA;
 	}
-	else if (x < mouseX) {
+	else if (x < last_mouse_x) {
 		frameRotationY -= OFFSET_CAMERA;
 	}
-	if (y > mouseY) {
+	if (y > last_mouse_y) {
 		frameRotationX += OFFSET_CAMERA;
 	}
-	else if (y < mouseY) {
+	else if (y < last_mouse_y) {
 		frameRotationX -= OFFSET_CAMERA;
 	}
-	mouseX = x;
-	mouseY = y;
+	last_mouse_x = x;
+	last_mouse_y = y;
 
 }
 
-#define MOVE_OFFSET 0.1f
 
 void onKey(unsigned char key, int x, int y) {
 	if (key == 'w') {
@@ -478,10 +405,6 @@ void onKey(unsigned char key, int x, int y) {
 	}
 }
 
-void onArrows(int key, int x, int y) {
-
-}
-
 /////////////////////////////////////////////////////////////////////// SETUP
 
 void setupCallbacks()
@@ -494,7 +417,6 @@ void setupCallbacks()
 	glutKeyboardFunc(onKey);
 	glutMouseFunc(onMouse);
 	glutMotionFunc(onMotion);
-	glutSpecialFunc(onArrows);
 }
 
 void checkOpenGLInfo()
@@ -563,7 +485,7 @@ void init(int argc, char* argv[])
 	createMeshes();
 	checkOpenGLError("ERROR: Could not create meshes (VAOs and VBOs).");
 	createShaderProgram();
-	//createBufferObjects();
+	checkOpenGLError("ERROR: Could not create shaders.");
 	createScene();
 	createAnimations();
 	setupCallbacks();
